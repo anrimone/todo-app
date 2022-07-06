f270216efb13f41f29c159ed67606f116cb21157
anrimone <2310_96@inbox.ru>
2022-07-06 21:44:59 +0600

    new

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
   <div id="app">
     <todo-app></todo-app>
   </div>
+  <div id="book">
+    <quote-book></quote-book>
+  </div>
+
 </template>
 
 <script>
 import TodoApp from './components/TodoApp.vue'
+import QuoteBook from './components/QuoteBook.vue'
 
 export default {
   name: 'App',
   components: {
-    TodoApp
+    TodoApp,
+    QuoteBook
   }
 }
 </script>
--- src/components/QuoteBook.vue
+++ src/components/QuoteBook.vue
@@ -1,46 +1,32 @@
 <template>
   <div class="container" style="max-width: 600px">
-    <h2 class="text-center mt-5">Ту-ду листик</h2>
+    <h2 class="text-center mt-5">Цитатник</h2>
     <div class="d-flex mt-5">
-      <input
-        type="text"
-        v-model="task"
-        placeholder="Введите задачу"
-        class="w-100 form-control"
-      />
-      <button class="btn btn-warning rounded-0" @click="submitTask">
+    <textarea
+      type="text"
+      v-model="task"
+      @keydown.enter.exact.prevent="submitQuoteBook"
+      class="w-100 form-control"
+    ></textarea>
+     <button class="btn btn-warning rounded-0" @click="submitTask">
         Отправить
       </button>
     </div>
+    
     <table class="table table-bordered mt-5">
       <thead>
         <tr>
-          <th scope="col">Задачи</th>
-          <th scope="col" style="width: 120px">Статус</th>
-          <th scope="col" class="text-center">#</th>
-          <th scope="col" class="text-center">#</th>
+          <th scope="col">Цитаты</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(task, index) in tasks" :key="index">
-          <td>
+          <td class="bodyblock">
             <span :class="{ 'line-through': task.status === 'finished' }">
               {{ task.name }}
             </span>
           </td>
-          <td>
-            <span
-              class="pointer noselect"
-              @click="changeStatus(index)"
-              :class="{
-                'text-danger': task.status === 'to-do',
-                'text-success': task.status === 'finished',
-                'text-warning': task.status === 'in-progress',
-              }"
-            >
-              {{ capitalizeFirstChar(task.status) }}
-            </span>
-          </td>
+      
           <td class="text-center">
             <div @click="deleteTask(index)">
               <span class="fa fa-trash pointer"></span>
@@ -68,49 +54,23 @@ export default {
     return {
       task: "",
       editedTask: null,
-      statuses: ["to-do", "in-progress", "finished"],
 
       tasks: [
         {
-          name: "Опозориться.",
-          status: "to-do",
+          name: "Господь, создавая человека, несколько переоценил свои силы. Оскар Уайльд",
         },
         {
-          name: "Процесс позора.",
-          status: "in-progress",
+          name: "Будь собой, остальные роли заняты. Оскар Уайльд",
         },
         {
-          name: "Опозорились.",
-          status: "finished",
+          name: "Весь мир — театр, но труппа никуда не годится. Оскар Уайльд",
         },
       ],
     };
   },
 
   methods: {
-    
-    capitalizeFirstChar(str) {
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    },
-
- 
-    changeStatus(index) {
-      let newIndex = this.statuses.indexOf(this.tasks[index].status);
-      if (++newIndex > 2) newIndex = 0;
-      this.tasks[index].status = this.statuses[newIndex];
-    },
-
-
-    deleteTask(index) {
-      this.tasks.splice(index, 1);
-    },
-
-    editTask(index) {
-      this.task = this.tasks[index].name;
-      this.editedTask = index;
-    },
-
-    submitTask() {
+     submitTask() {
       if (this.task.length === 0) return;
 
    
@@ -127,11 +87,45 @@ export default {
 
       this.task = "";
     },
+    capitalizeFirstChar(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+
+ 
+
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+
+    submitQuoteBook() {
+      if (this.task.length === 0) return;
+      if (this.editedTask != null) {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      } else {
+        this.tasks.unshift({
+          name: this.task,
+          status: "todo",
+        });
+      }
+      this.task= "";
+    },
   },
 };
 </script>
 
 <style scoped>
+.bodyblock {
+  border-radius: .5rem;
+box-shadow: 0 3px 9px rgba(0,0,0,.082),0 2px 3px rgba(0,0,0,.05);
+padding: 2em;
+}
 .btn-warning {
     margin-left: 20px;
 }
